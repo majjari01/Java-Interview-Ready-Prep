af9385e7f087016e3a271c8b901535fe92d91cad
majjari01 <maheshwarimajjari55@gmail.com>
2025-08-13 20:24:06 +0530

    lamnda expression

---
+++ src/java8/lamdaexpression/LamdaExpressionClass.java
@@ -0,0 +1,9 @@
+package java8.lamdaexpression;
+
+public class LamdaExpressionClass {
+    public static void main(String[] args){
+        LamdaExpression impl1=(input)->System.out.println("Hi from Lamda Expression to:"+ input);
+        impl1.abstractMethod("Maheshwari");
+        LamdaExpression impl2=(input)->System.out.println("this is another impkementation without using @override say hii:"+input);
+impl2.abstractMethod("Maheshwari");}
+}
